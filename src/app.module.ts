f3221dcdb4db7a96a8144f4ede2fdfbc56029818
KHUN DECH <koondet420@gmail.com>
2026-03-23 15:25:34 +0700

    feat(columns): generate columns resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,17 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { BoardsModule } from './boards/boards.module';
+import { ColumnsModule } from './columns/columns.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, AuthModule, workspacesModule, BoardsModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    AuthModule,
+    workspacesModule,
+    BoardsModule,
+    ColumnsModule,
+  ],
 
   controllers: [AppController],
   providers: [AppService],

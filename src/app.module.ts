fd6c390a73514d20adf34b331ccb989810a16e2d
khundech1 <koondet420@gmail.com>
2026-03-23 16:21:41 +0700

    Feature/columns resource (#2)
    * feat(boards): generate boards resource (#1)
    
    * feat(columns): generate columns resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
+import { BoardsModule } from './boards/boards.module';
+import { ColumnsModule } from './columns/columns.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, AuthModule, workspacesModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    AuthModule,
+    workspacesModule,
+    BoardsModule,
+    ColumnsModule,
+  ],
 
   controllers: [AppController],
   providers: [AppService],
